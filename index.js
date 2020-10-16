0ce15c3b6526551986c7695cbabf67a35f61ec94
Nathan P <24nputra11@isd77.org>
2020-10-16 16:28:02 +0000

    added a bot.login so the bot can actually interface with the code

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const bot = new Discord.Client();
 
 const token = process.env.BOT_TOKEN
 
+const keepalive = require('./keepalive.js')
+
 console.log('Hello World')
 bot.on('ready', () => {
   console.log(bot.user.username + 'is alive')
@@ -13,6 +15,4 @@ bot.on('message', message => {
   console.log(message.author.username + ' said ' + message.content)
 })
 
-
-
-
+bot.login(token)

5a4d960556086cc3f67f8858ca194ad3eec52baf
Nathan P <24nputra11@isd77.org>
2020-10-16 16:32:44 +0000

    added comments for clarity

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ const token = process.env.BOT_TOKEN
 
 const keepalive = require('./keepalive.js')
 
-console.log('Hello World')
+//init bot
 bot.on('ready', () => {
   console.log(bot.user.username + 'is alive')
 });
 
+//when someone says something, console.logs it
 bot.on('message', message => {
   console.log(message.author.username + ' said ' + message.content)
 })

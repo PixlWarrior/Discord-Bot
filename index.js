031b6098bafc377c06c3ef5a24332a6da4270426
Nathan P <24nputra11@isd77.org>
2020-10-19 22:45:07 +0000

    added variable prefix, for ease of use

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const token = process.env.BOT_TOKEN
 
 const keepalive = require('./keepalive.js')
 
+const pf = "$"
+
 //init bot
 bot.on('ready', () => {
   console.log(bot.user.username + ' is alive')
@@ -16,4 +18,72 @@ bot.on('message', message => {
   console.log(message.author.username + ': ' + message.content)
 })
 
-bot.login(token)
+bot.on('message', message => {
+  if(!message.guild) return;
+
+  if(message.content.startsWith(pf + 'kick')) {
+
+    const user = message.mentions.users.first();
+
+    if(user) {
+      
+      const member = message.guild.member(user);
+
+      if(member) {
+
+        member
+        .kick()
+        .then(() => {
+          message.reply(`succesfully kicked ${user.tag}`);
+        })
+        .catch(err => {
+          message.reply('I was unable to kick the member');
+
+          console.error(err);
+        })
+      }
+      else {
+        message.reply('That member isn\'t in this guild!');
+      }
+    }
+    else {
+      message.reply('You didn\'t mention the user you wanted to kick!')
+    }
+  }
+});
+
+bot.on('message', message => {
+  if(!message.guild) return;
+
+  if(message.content.startsWith(pf + 'ban')) {
+
+    const user = message.mentions.users.first();
+
+    if(user) {
+      
+      const member = message.guild.member(user);
+
+      if(member) {
+
+        member
+        .ban()
+        .then(() => {
+          message.reply(`succesfully banned ${user.tag}`);
+        })
+        .catch(err => {
+          message.reply('I was unable to ban the member');
+
+          console.error(err);
+        })
+      }
+      else {
+        message.reply('That member isn\'t in this guild!');
+      }
+    }
+    else {
+      message.reply('You didn\'t mention the user you wanted to ban!')
+    }
+  }
+});
+
+bot.login(token)         
--- keepalive.js
+++ keepalive.js
@@ -1,6 +1,6 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
-  res.write('Server is online')
+  res.write('Server is online, That is good.')
   res.end()
 }).listen(8080);
